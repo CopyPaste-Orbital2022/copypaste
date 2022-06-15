dce5f7a04474f4599c34fc2312c4f9f5468823a6
Xiuxuan Wang <xiuxuan@Xiuxuans-MacBook-Air.local>
2022-06-15 20:24:38 +0800

    modified tests

--- test/features/drawing/presentation/bloc/drawing_bloc/drawing_bloc_test.dart
+++ test/features/drawing/presentation/bloc/drawing_bloc/drawing_bloc_test.dart
@@ -3,7 +3,5 @@ import 'package:flutter/widgets.dart';
 import 'package:copypaste/features/drawing/presentation/bloc/drawing_bloc/drawing_bloc.dart';
 
 void main() {
-
-	// Write tests for features/drawing/presentation/bloc/drawing_bloc/drawing_bloc.dart here.
-
+  // Write tests for features/drawing/presentation/bloc/drawing_bloc/drawing_bloc.dart here.
 }
